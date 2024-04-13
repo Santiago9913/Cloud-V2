94a56cc6e5b1c163b92bb4cf3a1fe9e43708717b
Santiago Fajardo <s.fajardot@uniandes.edu.co>
2024-04-13 12:52:08 -0500

    fix video

--- env.py
+++ env.py
@@ -3,7 +3,7 @@ import os
 POSTGRES_USER = os.getenv("POSTGRES_USER", default="postgres")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", default="password")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", default="localhost")
-POSTGRES_DB = os.getenv("POSTGRES_DB", default="dbIDRL")
+POSTGRES_DB = os.getenv("POSTGRES_DB", default="dbidrl")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", default=4345)
 
 JWT_SECRET_KEY = os.getenv("JWT_SECRET", default="MISW-4204")
