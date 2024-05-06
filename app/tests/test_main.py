e25ee49a5a1b386f4921eec165f226222db91e69
Santiago Fajardo <s.fajardot@uniandes.edu.co>
2024-05-06 11:35:00 -0500

    test

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -2,6 +2,10 @@ from sqlmodel import create_engine, Session, SQLModel
 from app.main import app
 from utils.db_engine import get_session
 from fastapi.testclient import TestClient
+import pytest
+from httpx import AsyncClient
+from jose import jwt
+from env import JWT_SECRET_KEY, ALGORITHM
 
 
 sqlite_file_name = "database.db"
@@ -29,7 +33,7 @@ app.dependency_overrides[get_session] = override_get_session
 client = TestClient(app)
 
 
-def test_signin():
+def test_create_user():
     response = client.post(
         "/auth/signup",
         json={
@@ -40,23 +44,22 @@ def test_signin():
     )
 
     assert response.status_code == 200
+    print(response.json())
     data = response.json()
 
     assert data["name"] == "Test"
     assert data["email"] == "test@test.com"
 
 
-def test_signin():
-    response = client.post(
-        "/auth/signin",
-        json={"email": "test@test.com", "password": "test"},
+def test_get_user():
+    token = jwt.encode(
+        {"email": "test@test.com"},
+        key=JWT_SECRET_KEY,
+        algorithm=ALGORITHM,
+    )
+    response = client.get(
+        "/users",
+        headers={"Authorization": f"Bearer {token}"},
     )
     print(response.json())
     assert response.status_code == 200
-    data = response.json()
-    assert "access_token" in data
-    assert "token_type" in data
-    assert "user" in data
-    assert data["user"]["email"] == "test@test.com"
-    assert data["user"]["name"] == "Test"
-    assert "id" in data["user"]
