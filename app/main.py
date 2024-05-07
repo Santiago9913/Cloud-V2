13c979cba25b6ff7cc2c41fd04898adf6f179d33
Santiago Fajardo <s.fajardot@uniandes.edu.co>
2024-05-07 01:30:14 -0500

    test deploy

--- app/main.py
+++ app/main.py
@@ -41,4 +41,4 @@ app.include_router(users_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run("main:app", port=int(os.environ.get("PORT", 8000)), host="0.0.0.0")
