4d62be5994543f69e81d6810b480a03005daea30
Santiago Fajardo <s.fajardot@uniandes.edu.co>
2024-05-23 12:34:10 -0500

    more logs

--- app/utils/pub_sub.py
+++ app/utils/pub_sub.py
@@ -6,13 +6,17 @@ from env import TOPIC_NAME
 
 
 def publish_message(data):
-    # if os.environ.get("GOOGLE_APPLICATION_CREDENTIALS") is None:
-    #     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
-    data = json.dumps(data)
-    print(data)
-    publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path("cloud-dev-421516", TOPIC_NAME)
-    data = data.encode("utf-8")
-    print(data)
-    future = publisher.publish(topic_path, data)
-    return future.result()
+    try:
+        # if os.environ.get("GOOGLE_APPLICATION_CREDENTIALS") is None:
+        #     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+        data = json.dumps(data)
+        print(data)
+        publisher = pubsub_v1.PublisherClient()
+        topic_path = publisher.topic_path("cloud-dev-421516", "process_video_v2")
+        data = data.encode("utf-8")
+        print(data)
+        future = publisher.publish(topic_path, data)
+        return future.result()
+    except Exception as e:
+        print(e)
+        raise Exception("Error publishing message")
